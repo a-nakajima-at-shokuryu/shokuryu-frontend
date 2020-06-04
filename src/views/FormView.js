b73d69ca5da0d843036c626fa4c09681d022393b
a-nakajima <a-nakajia@shokuryu.co.jp>
2020-06-04 17:57:02 +0900

    商品セレクタ実装開始

--- src/views/FormView.js
+++ src/views/FormView.js
@@ -4,9 +4,12 @@ import * as Yup from 'yup';
 import { Formik, Form } from 'formik'; 
 import { addDays } from 'date-fns'; 
 import { makeStyles } from '@material-ui/core';
+import ShohinSelector from '../components/ShohinSelector';
 
 const useStyles = makeStyles(theme => ({
-  root: {}, 
+  root: {
+    
+  }, 
 }));
 
 const days = 3; 
@@ -21,6 +24,7 @@ const validationSchema = Yup.object().shape({
 
 const initialValues = {
   date: new Date(), 
+  shohin: {}, 
 };
 
 const FormView = () => {
@@ -44,6 +48,12 @@ const FormView = () => {
             name={'date'}
             onChange={date => props.setFieldValue('date', date)}
             label="納品日"
+          />
+
+          <ShohinSelector
+            label="商品CD"
+            value={props.values.shohin}
+            onChange={value => props.setFieldValue('shohin', value)}
           />
           <pre>{JSON.stringify(props.values, null, 2)}</pre>
           <pre>{JSON.stringify(props.errors, null, 2)}</pre>
